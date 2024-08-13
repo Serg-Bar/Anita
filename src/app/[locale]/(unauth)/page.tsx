e35c3b2f104d52050efcacda962ce5c93834e073
serg-bar <sergeibarats@gmail.com>
2024-08-13 21:32:46 +0300

    fix

--- src/app/[locale]/(unauth)/page.tsx
+++ src/app/[locale]/(unauth)/page.tsx
@@ -15,16 +15,5 @@ export async function generateMetadata(props: { params: { locale: string } }) {
 export default function Index(props: { params: { locale: string } }) {
   unstable_setRequestLocale(props.params.locale);
 
-  return (
-    <p>
-      Anita{' '}
-      <a
-        className="text-blue-700 hover:border-b-2 hover:border-blue-700"
-        href="https://nextjs-boilerplate.com/pro-saas-starter-kit"
-      >
-        Next.js Boilerplate SaaS
-      </a>{' '}
-      can help you build one.
-    </p>
-  );
+  return <p>Anita </p>;
 }
